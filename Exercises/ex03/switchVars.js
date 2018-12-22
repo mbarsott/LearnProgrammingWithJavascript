f1503e3bc17c7a4957c03c36dbf2f250c5757a16
Marcelo Barsotti <mbarsott@gmail.com>
2018-12-22 17:28:20 -0500

    Add simple exercises

---
+++ Exercises/ex03/switchVars.js
@@ -0,0 +1,15 @@
+// Write a function switchVars that takes two global variables, a and b, and invert their values.
+// You should write your code in place of the comment below that reads // place your code here
+// The program below should produce the following output:
+// a = 3; b = 5
+// a = 5; b = 3
+
+var a=3,b=5;
+
+console.log("a = "+a+"; b = "+b);
+switchVars();
+console.log("a = "+a+"; b = "+b);
+
+function switchVars() {
+// place your code here
+}
---
+++ Exercises/ex04/program.js
@@ -0,0 +1,18 @@
+// Write a function switchArray that takes and array with two elements and invert their values
+// You should write your code in place of the comment below that reads // place your code here
+// The program below should produce the following output:
+// Array(2) [3, 5]
+// Array(2) [5, 3]
+
+program();
+
+function program() {
+    var a = [3, 5];
+    console.log (a);
+    switchArray(a);
+    console.log (a);
+}
+
+function switchArray(a) {
+// place your code here
+}
