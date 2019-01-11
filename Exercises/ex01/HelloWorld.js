b67289618a5f90fd032f1480ac60d541ae2f0c0c
Marcelo Barsotti <mbarsott@gmail.com>
2019-01-10 20:56:07 -0500

    Hello World!

--- Exercises/ex01/HelloWorld.js
+++ Exercises/ex01/HelloWorld.js
@@ -1,6 +1,6 @@
 // Write a program that prints the string "Hello, World!" using the console.log function
 // You should write your code in place of the comment below that reads // place your code here
 // The program below should produce the following output:
-// Hello, World!
+// Hello, World! 
 
 // place your code here
